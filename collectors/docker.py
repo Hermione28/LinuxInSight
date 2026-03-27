202b3bc7cb62ffe11e4d1403a5dbacf6cb0f53ca
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-27 14:42:58 +0530

    Create docker.py

---
+++ collectors/docker.py
@@ -0,0 +1,14 @@
+import docker
+
+client = docker.from_env()
+
+def get_containers():
+    containers = []
+    
+    for c in client.containers.list():
+        containers.append({
+            "name": c.name,
+            "status": c.status
+        })
+    
+    return containers
