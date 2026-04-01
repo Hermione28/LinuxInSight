eb11cdf72dc756c8e57c563b7d30e536a137dea1
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-01 12:01:38 +0530

    Update process.py

--- collectors/process.py
+++ collectors/process.py
@@ -3,16 +3,17 @@ import psutil
 def get_top_processes(limit=5):
     processes = []
 
-    for proc in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent']):
-        try:
+    try:
+        for proc in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent']):
             processes.append({
                 "pid": proc.info['pid'],
                 "name": proc.info['name'],
                 "cpu": proc.info['cpu_percent'],
                 "memory": round(proc.info['memory_percent'], 2)
             })
-        except:
-            continue
 
-    processes = sorted(processes, key=lambda x: x['cpu'], reverse=True)
-    return processes[:limit]
+        processes = sorted(processes, key=lambda x: x['cpu'], reverse=True)
+        return processes[:limit]
+
+    except:
+        return []
