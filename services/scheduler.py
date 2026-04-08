1369dca0b27d7f4b4d6a4345afd205b1d835d838
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-08 19:53:29 +0530

    Update scheduler.py

--- services/scheduler.py
+++ services/scheduler.py
@@ -1,3 +1,4 @@
+from collectors import cpu, disk, memory, network
 from services.metrics_service import get_all_metrics
 from database import insert_metrics
 from services.alert_service import check_alerts
@@ -5,11 +6,16 @@ import time
 
 def start_scheduler():
     while True:
-        data = get_all_metrics()   # ✅ correct
+        data = get_all_metrics()   
 
-        insert_metrics(data)
+        insert_metrics(
+    data["cpu"],
+    data["memory"],
+    data["disk"],
+    data["network"]
+)
 
-        alerts = check_alerts(data)
+        alerts = check_alerts(data) 
         if alerts:
             print("ALERT:", alerts)
 
